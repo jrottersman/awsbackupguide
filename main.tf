c1fa19d8afea0d1aec8cb92a939fe09e5b283b29
Jake Rottersman <rottersmanjake@gmail.com>
2023-08-19 20:47:38 -0700

    IAM role and policy attach

--- main.tf
+++ main.tf
@@ -7,3 +7,29 @@ resource "aws_backup_vault" "example" {
   kms_key_arn = aws_kms_key.example.arn
 }
 
+data "aws_iam_policy" "backup_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
+}
+
+resource "aws_iam_role" "backup_service_role" {
+  name = "backup_service_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "backup.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "backup-attach" {
+  role       = aws_iam_role.backup_service_role.name
+  policy_arn = data.aws_iam_policy.backup_policy.arn
+}
+
